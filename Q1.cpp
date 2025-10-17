5604fb2693b11578ec140102a870d372ff364f0a
mems250005013-tech <mems250005013@iiti.ac.in>
2025-10-17 15:57:25 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+struct item{
+int id[6];
+float cost[6];
+};
+int main(){
+  item display;
+display.id[0]=1;
+display.id[1]=2;
+display.id[2]=3;
+display.id[3]=4;
+display.id[4]=5;
 
+display.cost[0]=35;
+display.cost[1]=65;
+display.cost[2]=25;
+display.cost[3]=75;
+display.cost[4]=45;
+
+cout<< " Enter the properties of items"<<endl;
+for(int i=0;i<5;i++){
+if
