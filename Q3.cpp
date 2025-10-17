4a140b17390e08404f8c3237f8cbffa8d9d63c0c
mems250005013-tech <mems250005013@iiti.ac.in>
2025-10-17 16:19:09 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+using namespace std;
+struct point{
+    float x[7];
+    float y[7];
+};
+int main()
+{
+    point quadrant;
+    for(int i=0;i<7;i++){
+        cout<<"Enter the co ordinates of the "<<i+1<<" point"<<endl;
+        cin>>quadrant.x[i];
+        cin>>quadrant.y[i];
+    }
+    int count=0;
+    for(int i=0;i<7;i++){
+        if(quadrant.x[i]>0 && quadrant.y[i]>0){
+            count++;
+        }
+    }
+    cout<<"Total numbers of the points in first quadrant are = "<<count;
+}
